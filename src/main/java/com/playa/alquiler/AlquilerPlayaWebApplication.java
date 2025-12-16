3229b544408045a2b1270770241cd889e7e57389
Fermín <cristotai52@gmail.com>
2025-12-15 23:37:52 -0500

    sin fxml

---
+++ src/main/java/com/playa/alquiler/AlquilerPlayaWebApplication.java
@@ -0,0 +1,13 @@
+package com.playa.alquiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlquilerPlayaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlquilerPlayaWebApplication.class, args);
+    }
+
+}
